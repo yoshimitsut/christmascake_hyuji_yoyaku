da2344c6abeb9726f0843d6ed6da81fefd6daab3
yoshimitsut <shimitsutanaka@gmail.com>
2025-11-14 11:04:59 -0300

    arrumando bug de excel

--- backend/src/server.js
+++ backend/src/server.js
@@ -578,6 +578,7 @@ app.get('/api/list', async (req, res) => {
     let query = `
       SELECT 
         o.*, 
+        o.created_at as date_order,
         oc.id AS order_cake_id,
         oc.cake_id,
         c.name AS cake_name,
